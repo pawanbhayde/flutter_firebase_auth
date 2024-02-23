6dcb9d44c8f6a846aeed3e5a29dfaf99680e7e0b
Pawan Bhayde <pawanbhayde721@gmail.com>
2024-02-23 22:12:15 +0530

    update code

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -7,66 +7,67 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Add a Image logo here
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: Image(
+          child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Add a Image logo here
+
+            const Image(
               image: AssetImage('images/logo.png'),
               height: 200,
             ),
-          ),
-          // Add a Text widget here
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
+            const SizedBox(
+              height: 20,
+            ),
+            // Add a Text widget here
+            const Text(
               'Welcome to our app',
               style: TextStyle(fontSize: 20),
             ),
-          ),
-          // Add a Email Textfield here
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
+            const SizedBox(
+              height: 20,
+            ),
+            // Add a Email Textfield here
+            const TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Email',
               ),
             ),
-          ),
-          // Add a Password Textfield here
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: TextField(
+            const SizedBox(
+              height: 20,
+            ),
+            // Add a Password Textfield here
+            const TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Password',
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          // Add a Login Button here
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.all(20),
-                backgroundColor: Colors.deepPurple,
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              child: const Text(
-                'Login',
-                style: TextStyle(fontSize: 20, color: Colors.white),
+            const SizedBox(
+              height: 20,
+            ),
+            // Add a Login Button here
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  backgroundColor: Colors.deepPurple,
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       )),
     );
   }
